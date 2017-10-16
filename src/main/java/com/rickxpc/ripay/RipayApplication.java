eee141728c84d5538cab1ca2d3798c8ef59ce9a4
a021672 <cloudxpc@126.com>
2017-10-16 13:55:41 +0800

    bower, gulp and initial files

---
+++ src/main/java/com/rickxpc/ripay/RipayApplication.java
@@ -0,0 +1,11 @@
+package com.rickxpc.ripay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RipayApplication.class, args);
+	}
+}
